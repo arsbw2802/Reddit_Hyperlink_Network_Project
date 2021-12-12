fce6da0edcee6b4b52a4294604dc767f950149cf
Kartik Mehra <kartikm3@illinois.edu>
2021-12-12 16:37:22 -0600

    Updated files

--- graph.h
+++ graph.h
@@ -11,24 +11,57 @@ using std::vector;
 using std::string;
 using std::map;
 
+typedef std::pair<int, int> vPair;
 
 class Graph {
 	public:
+		/**
+           * Constructor for Graph class that constructs graph instance based on target reddits, source reddits, and sentiments
+           * Initialize the _AdjacencyMatrix and map_UniqueID (by calling assignID()) private variables
+           * @param target - vector of all target nodes
+		   * @param source - vector of all source nodes
+		   * @param sentiment - vector of sentiments
+        **/
 		Graph(vector<Vertex> target, vector<Vertex> source, vector<int> sentiment);
 		
-		// create a map
+		/**
+           * Initialize map_UniqueID/ assign uniqueIDs to all the vertices from target and source
+           * @param target - vector of all target nodes
+		   * @param source - vector of all source nodes
+		   * @param sentiment - vector of sentiments
+        */
 		void assignID(vector<Vertex> target, vector<Vertex> source);
 
+		/**
+           * Find adjacent vertices of a given vertex and a flag (outgoing/ incoming)
+           * @param vertex - the vertex from which to find the incoming or outgoing nodes
+		   * @param flag - Decides whether to find outgoing or incoming nodes
+           * @return a vector of adjacent vertices of the specified vertex
+        */
 		vector<int> findAdjacentVertices(int vertex, int flag);
 
-		Vertex pageRank(int max_iteration);
+		/**
+           * Given a certain number of iterations, the function will compute the pageRank value of every vertex within the graph
+           * @param max_iteration- the number of iterations
+           * @return a vector of pageRank values of all the vertices within the graph, indexed by the uniqueIDs of the vertices
+        */
+		vector<double> pageRank(int max_iteration);
+
+		/**
+           * Given a the uniqueID of the start vertex, the function will compute the shortest distance from every other vertices
+           * within the graph to that start vertex
+           * @param startID - the uniqueID of the starting node
+           * @return a vector of distances, indexed by the uniqueIDs of the vertices
+        */
+		vector<int> dijkstra(int startID);
 
 		void printMatrix();
 
 	private:
 		// adjacency matrix (2D vector of edge pointers)
 		vector<vector<Edge*>> _AdjacencyMatrix;
-	
+
+		// map subreddits name to uniqueIDs
 		map<string, int> map_UniqueID;
 
 
--- tests.cpp
+++ tests.cpp
@@ -33,3 +33,29 @@ TEST_CASE("Page rank 1") {
     REQUIRE(ranks[2] ==  0.125);
     REQUIRE(ranks[3] == Approx(0.33333333));
 }
+
+TEST_CASE("Dijkstra") {
+    vector<string> source_data = {"A", "A", "D", "D", "B","B", "E"};
+	vector<string> target_data = {"B", "D", "B", "E", "E","C", "C"};
+	vector<string> sentiment_data = {"3", "1", "1", "3", "1","3", "1"};
+	vector<Vertex> source, target;
+	vector<int> sentiment;
+	
+	
+	
+	for (unsigned i = 0; i < source_data.size(); i++) {
+		source.push_back(Vertex(source_data[i]));			// convert source and target strings to Vertices
+		target.push_back(Vertex(target_data[i]));
+		sentiment.push_back(stoi(sentiment_data[i]));
+		
+	}
+
+	Graph short_graph = Graph(target, source, sentiment);
+
+    vector<int> ranks = short_graph.dijkstra(4);
+    REQUIRE(ranks[0] == 2);
+    REQUIRE(ranks[1] ==  1);
+    REQUIRE(ranks[2] ==  3);
+    REQUIRE(ranks[3] == 4);
+	REQUIRE(ranks[4] == 0);
+}
